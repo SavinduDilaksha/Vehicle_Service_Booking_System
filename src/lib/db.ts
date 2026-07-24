a3c57b4d3a7c7451fb5631c62fb6a3ae67a9474f
savindu.dilaksha147@gmail.com <savindu.dilaksha147@gmail.com>
2026-07-24 17:47:05 +0530

    .

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,1 +1,15 @@
-// TODO: Implementation
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const db = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
